d37ab9093813e0a34c87de9c11709236e206d56c
M00N77 <kplatonglfc@gmail.com>
2026-04-09 00:41:22 +0300

    feat: mvp

--- src/components/PromoImage.jsx
+++ src/components/PromoImage.jsx
@@ -5,7 +5,7 @@ import promoImageMobile320 from '../img/design-mobile-320.png'
 export default function PromoImage() {
   return (
     <div className="flex w-full justify-center min-[970px]:h-full min-[970px]:justify-start">
-      <picture className="block w-full max-w-[210px] shrink-0 min-[970px]:flex min-[970px]:h-full min-[970px]:max-w-[300px] min-[970px]:items-end xl:max-w-[420px]">
+      <picture className="block w-full max-w-[210px] shrink-0 min-[970px]:flex min-[970px]:h-full n-[970px]:max-w-[300px] min-[970px]:items-end xl:max-w-[420px]">
         <source media="(max-width: 320px)" srcSet={promoImageMobile320} />
         <source media="(max-width: 375px)" srcSet={promoImageMobile375} />
         <img
